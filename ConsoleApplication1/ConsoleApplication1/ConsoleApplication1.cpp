2df3254ed1444315deaadfc773513b0d5b01f8a1
rnrytjr2 <43197846+rnrytjr2@users.noreply.github.com>
2022-10-20 22:18:34 +0900

    solved

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,83 @@
+﻿// ConsoleApplication1.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+int main()
+{
+	while (true)
+	{
+		stack<char> PSs;
+		string balancestring;
+		cin.ignore();
+		getline(cin, balancestring, '.');
+
+		if (balancestring.size() == 0)
+		{
+			break;
+		}
+
+		for (int i = 0; i < balancestring.size(); i++)
+		{
+			if (balancestring[i] == '[')
+			{
+				PSs.push(balancestring[i]);
+			}
+			else if (balancestring[i] == ']')
+			{
+				if (PSs.size() > 0)
+				{
+
+					if (PSs.top() == '[')
+					{
+						PSs.pop();
+					}
+					else
+					{
+						PSs.push('!');
+						break;
+					}
+				}
+				else
+				{
+					PSs.push('!');
+					break;
+				}
+			}
+			else if (balancestring[i] == '(')
+			{
+				PSs.push(balancestring[i]);
+			}
+			else if (balancestring[i] == ')')
+			{
+				if (PSs.size() > 0)
+				{
+					if (PSs.top() == '(')
+					{
+						PSs.pop();
+					}
+					else
+					{
+						PSs.push('!');
+						break;
+					}
+				}
+				else
+				{
+					PSs.push('!');
+					break;
+				}
+			}
+		}
+		if (PSs.size() == 0)
+		{
+			cout << "yes" << '\n';
+		}
+		else
+		{
+			cout << "no" << '\n';
+		}
+	}
+}
